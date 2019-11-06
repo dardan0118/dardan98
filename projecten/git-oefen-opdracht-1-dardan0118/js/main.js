9c9ccbdc969d8db0c7304fe187c32bc40f1e6316
dardan0118 <56684225+dardan0118@users.noreply.github.com>
2019-11-06 15:52:42 +0100

    Add files via upload
    test 2

---
+++ projecten/git-oefen-opdracht-1-dardan0118/js/main.js
@@ -0,0 +1,15 @@
+
+function setDate()
+{
+  var d = new Date();
+
+document.getElementById("demo").innerHTML = d;
+}
+
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
+
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
